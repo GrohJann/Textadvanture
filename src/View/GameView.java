b8673fcd8136cc197a2bff4745fec14f6e231b25
Jannis <jannismaxim@gmail.com>
2018-10-07 18:05:16 +0200

    GUI pics hinzugefügt; mit fxml experimentiert

--- src/View/GameView.java
+++ src/View/GameView.java
@@ -1,4 +1,33 @@
 package View;
 
-public class GameView extends MainView{
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class GameView extends Application {
+    
+    GameView(){
+    }
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("gameScene.fxml"));
+        primaryStage.setTitle("Textadventure");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+    
+    public void runButtonClicked(){
+    
+    }
+    
+    public void fleeButtonClicked(){
+    
+    }
+    
+    public void fightButtonClicked(){
+    
+    }
 }
