c0eda1d300364ac7ca8e1dc36adfc0e6f3f25bfa
Jannis <jannismaxim@gmail.com>
2018-10-08 22:00:17 +0200

    fensterrand entfernt

--- src/View/FXMLFiles/StartSceneController.java
+++ src/View/FXMLFiles/StartSceneController.java
@@ -4,6 +4,7 @@ import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.application.Platform;
 
 public class StartSceneController {
     @FXML private ImageView startBtn;
@@ -13,5 +14,11 @@ public class StartSceneController {
     public void btnClicked(Event e){
         startBtn.setImage(new Image("file:assets/pictures/gui_pics/Button/BtnClicked.png"));
         System.out.println("Button clicked");
+    }
+    
+    @FXML
+    public void exit(Event e){
+        System.out.println("Exit Button clicked");
+        Platform.exit();
     }
 }
