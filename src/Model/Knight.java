7afbed29298a3781ca0c06cee785e174024bcbbd
MDautoski <m.dautoski52@gmail.com>
2018-10-12 09:20:10 +0200

    javafx start

---
+++ src/Model/Knight.java
@@ -0,0 +1,10 @@
+package Model;
+
+public class Knight extends PlayerCharacter{
+    public Knight(){
+        setArmor(50);
+        setHealth(100);
+        setStrength(50);
+        setSpeed(50);
+    }
+}
