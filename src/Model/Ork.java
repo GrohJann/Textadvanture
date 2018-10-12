5358b13e17d17b47b75923f97daed344e8cb5af2
MDautoski <m.dautoski52@gmail.com>
2018-10-12 09:20:10 +0200

    javafx start

---
+++ src/Model/Ork.java
@@ -0,0 +1,10 @@
+package Model;
+
+public class Ork extends PlayerCharacter {
+    public Ork(){
+        setArmor(100);
+        setHealth(110);
+        setStrength(100);
+        setSpeed(20);
+    }
+}
