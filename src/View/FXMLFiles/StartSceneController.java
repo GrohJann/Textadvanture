75a548ff2e9df3b9491b24eff1db5f0d51bc9df2
grohjann <jannismaxim@gmail.com>
2018-10-12 09:19:52 +0200

    view update

--- src/View/FXMLFiles/StartSceneController.java
+++ src/View/FXMLFiles/StartSceneController.java
@@ -3,21 +3,26 @@ package View.FXMLFiles;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.application.Platform;
+import javafx.scene.layout.AnchorPane;
+
+import java.awt.*;
+
 
 public class StartSceneController {
-    @FXML private ImageView startBtn;
-    
-    
     @FXML
-    public void btnClicked(Event e){
-        startBtn.setImage(new Image("file:assets/pictures/gui_pics/Button/BtnClicked.png"));
+    private AnchorPane startView;
+
+
+    @FXML
+    public void btnClicked() {
         System.out.println("Button clicked");
+        startView.setVisible(false);
+
     }
     
     @FXML
-    public void exit(Event e){
+    public void exit(Event e) {
         System.out.println("Exit Button clicked");
         Platform.exit();
     }
