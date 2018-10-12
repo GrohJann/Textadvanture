172e33649d3c3fab46cf194a0724431aef551f52
grohjann <jannismaxim@gmail.com>
2018-10-12 09:19:52 +0200

    view update

--- src/View/StartView.java
+++ src/View/StartView.java
@@ -1,11 +1,21 @@
 package View;
 
+import View.FXMLFiles.StartSceneController;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.media.*;
+import java.io.File;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import java.io.IOException;
+import javafx.scene.image.ImageView;
+import javafx.stage.StageStyle;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class StartView extends Application {
     private Stage primaryStage;
@@ -18,16 +28,20 @@ public class StartView extends Application {
     public void start(Stage primaryStage) throws IOException{
         this.primaryStage = primaryStage;
         this.primaryStage.setTitle("Text Adventure");
-        this.showStartView();
+    
+        this.primaryStage.initStyle(StageStyle.UNDECORATED);
+        
+        View.FXMLLoader.loadFXML("FXMLFiles/startScene.fxml",this.primaryStage, this.mainLayout);
+        playMusic();
+        
     }
-    private void showStartView() throws IOException{
-        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(GameView.class.getResource("startScene.fxml"));
-        mainLayout = loader.load();
-        Scene scene = new Scene(mainLayout);
-        this.primaryStage.setScene(scene);
-        this.primaryStage.show();
+    
+    private void playMusic(){
+        String backgroundMusic = "src/assets/sounds/Ambient/Music/Epic_Journey.mp3";
+    
+        Media sound = new Media(new File(backgroundMusic).toURI().toString());
+        MediaPlayer mediaPlayer = new MediaPlayer(sound);
+        mediaPlayer.play();
     }
-
 }
 
