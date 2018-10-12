537e036702d75a2a7b3b51fcc3cd2333e9b95e79
MDautoski <m.dautoski52@gmail.com>
2018-10-12 09:21:00 +0200

    javafx start

--- src/Model/PlayerCharacter.java
+++ src/Model/PlayerCharacter.java
@@ -1,9 +1,5 @@
 package Model;
 
-public class PlayerCharacter extends Entity{
-      public PlayerCharacter(){
-          setArmor(20);
-          setHealth(100);
-          setAttack(50);
-      }
+public abstract class PlayerCharacter extends Entity {
+
 }
